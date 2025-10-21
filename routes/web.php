11023f76a21f9bf8a08fce8f09f72ee5153bfe15
Pasan24 <pramudithapasan076@gmail.com>
2025-10-21 10:51:58 +0530

    protect the routes

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,16 @@ use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\Auth\Logout;
 
 Route::get('/', [ChirpController::class, 'index']);
-Route::post('/chirps', [ChirpController::class, 'store']);
-Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
-Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
-Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+//protected routes
+Route::middleware('auth')->group(function(){
+
+    Route::post('/chirps', [ChirpController::class, 'store']);
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+    Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+    Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+});
 
 
 // Registration routes
